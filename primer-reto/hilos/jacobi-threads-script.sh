4bce2ca8a7f58c673f02e7a6e0f40111ad12645b
Johan Restrepo <johanestebanrestrepo@gmail.com>
2022-09-18 12:55:32 -0500

    Cambio a los tiempos de ejecución de scripts.
    Se cambiaron los tiempos de ejcución de los scripts de hilos y de
    procosos

--- primer-reto/hilos/jacobi-threads-script.sh
+++ primer-reto/hilos/jacobi-threads-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in 1000 5000 10000 20000 30000 40000 50000 60000
+for i in 5000 25000 50000 100000 150000 200000 250000 300000
 do 
   echo "Running script for array of length $i"
   for j in {1..10}
