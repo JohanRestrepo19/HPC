ee3c015b3e3ff1d4d707ec3e7cd83e079cb6badd
Johan Restrepo <johanestebanrestrepo@gmail.com>
2022-10-14 22:08:58 -0500

    Script para analisis de omp añadido

---
+++ tercer-laboratorio/script-omp.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+for i in 100 500 1000 2000 3000 4000 5000 6000
+do
+  echo "Ejecutando script para matrices de $i x $i"
+  for j in {1..10}
+  do
+    echo "iteracion $j"
+    ./matriz-omp.out $i $1 >> resultados-hilos-$1.csv
+  done
+done
