acab4112b5c8f65a350c5db3cfa189e25ff12763
Johan Restrepo <johanestebanrestrepo@gmail.com>
2022-10-15 19:25:36 -0500

    Modificaciones al script para el segundo-reto.
    Se modificó el script para que se ejecute las veces que sea necesario
    para la cantidad de hilos requerida.

--- segundo-reto/jacobi-omp-script.sh
+++ segundo-reto/jacobi-omp-script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-for i in 5000 25000 50000 100000 150000 200000 250000 300000
-do 
-  echo "Running script for array of length $i"
-  for j in {1..10}
+for cant_hilos in 4 8
+do
+  for i in 5000 25000 50000 100000 150000 200000 250000 300000
   do 
-    echo "iteration #$j"
-    ./jacobi-threads.out $i $i $1 >> resultado-hilos-$1.csv
+    echo "Running script for array of length $i"
+    for j in {1..10}
+    do 
+      echo "iteration #$j"
+      ./jacobi-threads.out $i $i $cant_hilos >> resultado-hilos-$cant_hilos.csv
+    done
   done
 done
