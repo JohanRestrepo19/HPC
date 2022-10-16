6d8b588a5a63a6fac534b44058854c96cbe3f770
Johan Restrepo <johanestebanrestrepo@gmail.com>
2022-10-16 09:56:12 -0500

    Cambios al script del segundo reto

--- segundo-reto/jacobi-omp-script.sh
+++ segundo-reto/jacobi-omp-script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-for cant_hilos in 4 8
+for number_threads in 4 8
 do
+  echo "Running for $number_threads threads"
   for i in 5000 25000 50000 100000 150000 200000 250000 300000
   do 
     echo "Running script for array of length $i"
     for j in {1..10}
     do 
       echo "iteration #$j"
-      ./jacobi-threads.out $i $i $cant_hilos >> resultado-hilos-$cant_hilos.csv
+      ./jacobi-omp.out $i $i $number_threads >> resultado-hilos-$number_threads.csv
     done
   done
 done
