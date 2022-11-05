d5808e0f1f144e6652319edc50ac456bf69a0579
Johan Restrepo <johanestebanrestrepo@gmail.com>
2022-11-05 17:28:46 -0500

    Avances implementación mpi

--- cuarto-laboratorio/matriz-mpi.c
+++ cuarto-laboratorio/matriz-mpi.c
@@ -42,34 +42,47 @@ int main(int argc, char *argv[]) {
   srand(time(0));
   int filas, columnas, idx_proceso, cant_procesos;
   int i, j, sum = 0;
+  int cant_elemen_por_proceso;
+  int *matriz_a, *matriz_b, *resultado;
 
   // Obtencion de los parametros que se pasan en el llamado al ejecutable
   filas = atoi(argv[1]);
   columnas = filas;
+
+  matriz_a = malloc_arreglo_1d_enteros(filas, columnas);
+  matriz_b = malloc_arreglo_1d_enteros(filas, columnas);
+  resultado = malloc_arreglo_1d_enteros(filas, columnas);
+  asignar_valores_aleatorios_arreglo(filas, columnas, matriz_a);
+  asignar_valores_aleatorios_arreglo(filas, columnas, matriz_b);
 
   MPI_Init(&argc, &argv);
 
   MPI_Comm_size(MPI_COMM_WORLD, &cant_procesos);
   MPI_Comm_rank(MPI_COMM_WORLD, &idx_proceso);
 
-  int *matriz_a;
+  cant_elemen_por_proceso = filas * columnas / cant_procesos;
 
   if (idx_proceso == 0) {
-    matriz_a = malloc_arreglo_1d_enteros(filas, columnas);
-    asignar_valores_aleatorios_arreglo(filas, columnas, matriz_a);
-    printf("Matriz inicial:\n");
+    printf("Matriz a desde proceso 0:\n");
     mostrar_arreglo_como_matriz(filas, columnas, matriz_a);
   }
 
-  int *matriz_a_temp = malloc((filas * columnas / cant_procesos) * sizeof(int));
+  MPI_Barrier(MPI_COMM_WORLD);
 
-  MPI_Scatter(matriz_a, (filas * columnas / cant_procesos), MPI_INT,
-              matriz_a_temp, (filas * columnas / cant_procesos), MPI_INT, 0,
-              MPI_COMM_WORLD);
+  if (idx_proceso == 1) {
+    printf("Matriz b desde proceso 1:\n");
+    mostrar_arreglo_como_matriz(filas, columnas, matriz_b);
+  }
+
+  int *matriz_a_temp = malloc(cant_elemen_por_proceso * sizeof(int));
+
+  MPI_Scatter(matriz_a, cant_elemen_por_proceso, MPI_INT, matriz_a_temp,
+              cant_elemen_por_proceso, MPI_INT, 0, MPI_COMM_WORLD);
+
+  MPI_Barrier(MPI_COMM_WORLD);
 
   printf("El procesos %d recibió los siguientes elementos: ", idx_proceso);
   mostrar_arreglo((filas * columnas / cant_procesos), matriz_a_temp);
-
   MPI_Finalize();
 
   return 0;
