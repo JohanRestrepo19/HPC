ca6a2577c2b7cfe9268c3283a9638ed93e958301
Johan Restrepo <johanestebanrestrepo@gmail.com>
2022-11-05 15:43:12 -0500

    MPI_Scatter funcionando.
    Con la matriz aplanada, ahora el MPI_Scatter funciona correctamente
    pasando la cantidad de valores justos a cada proceso.

--- cuarto-laboratorio/info.md
+++ cuarto-laboratorio/info.md
@@ -2,7 +2,7 @@
 
 ## Ideas
 
-- [ ] Utilizar las funciones MPI_Scatter() y MPI_Gather para repartir y reunir la información respectivamente
+- [x] Utilizar las funciones MPI_Scatter() y MPI_Gather para repartir y reunir la información respectivamente
       Nota: Al estar trabajando con matrices (arreglo de arreglos) se tiene que hallar la forma de pasar ese tipo de
       variable a las funciones para su correcto funcionamiento. :book:
 
--- cuarto-laboratorio/matriz-mpi.c
+++ cuarto-laboratorio/matriz-mpi.c
@@ -4,104 +4,73 @@
 #include <sys/time.h>
 #include <time.h>
 
-int **MATRIZ_A, **MATRIZ_B, **RESULTADO;
-
-int **malloc_arreglo_2d_enteros(int filas, int columnas) {
-  int **doble_puntero_entero;
-  doble_puntero_entero = (int **)malloc(filas * sizeof(int *));
-
-  for (int fila = 0; fila < filas; fila++) {
-    doble_puntero_entero[fila] = (int *)malloc(columnas * sizeof(int));
-  }
-
-  return doble_puntero_entero;
+int *malloc_arreglo_1d_enteros(int filas, int columnas) {
+  int *puntero_enteros;
+  puntero_enteros = malloc(filas * columnas * sizeof(int));
+  return puntero_enteros;
 }
 
-void liberar_arreglo_2d_enteros(int **doble_puntero_entero, int filas,
-                                int columnas) {
-  for (int fila = 0; fila < filas; fila++) {
-    free(doble_puntero_entero[fila]);
-  }
-  free(doble_puntero_entero);
-}
-
-void asignar_valores_aleatorios_matriz(int filas, int columnas, int **matriz) {
-  for (int fila = 0; fila < filas; fila++) {
-    for (int columna = 0; columna < columnas; columna++) {
-      matriz[fila][columna] = (rand() % 3) + 1;
-    }
+void asignar_valores_aleatorios_arreglo(int filas, int columnas, int *matriz) {
+  for (int idx = 0; idx < filas * columnas; idx++) {
+    matriz[idx] = (rand() % 3) + 1;
   }
 }
 
-void mostrar_matriz(int filas, int columnas, int **matriz) {
-  for (int fila = 0; fila < filas; fila++) {
-    for (int columna = 0; columna < columnas; columna++) {
-      printf("%d\t", matriz[fila][columna]);
-    }
-    printf("\n");
+void mostrar_arreglo(int cant_elementos, int *arreglo) {
+  for (int idx = 0; idx < cant_elementos; idx++) {
+    printf("%d\t", arreglo[idx]);
   }
   printf("\n");
 }
 
-void multiplicar_matrices(int filas, int columnas, int cant_procesos,
-                          int idx_proceso) {
-  int fila, columna, i, desde, hasta;
+void mostrar_arreglo_como_matriz(int filas, int columnas, int *arreglo) {
+  int contador_elementos = 0;
 
-  // calculo de filas a trabajar por el proceso
-  desde = idx_proceso * filas / cant_procesos;
-  hasta = (idx_proceso + 1) * filas / cant_procesos;
-
-  // Implementación de la funcion para multiplicar matrices
-  for (fila = desde; fila < hasta; fila++) {
-    for (columna = 0; columna < columnas; columna++) {
-      RESULTADO[fila][columna] = 0;
-      for (i = 0; i < columnas; i++) {
-        RESULTADO[fila][columna] += MATRIZ_A[fila][i] * MATRIZ_B[i][columna];
-      }
+  for (int idx = 0; idx < filas * columnas; idx++) {
+    printf("%d\t", arreglo[idx]);
+    contador_elementos++;
+    if (contador_elementos == columnas) {
+      printf("\n");
+      contador_elementos = 0;
     }
   }
+  printf("\n");
 }
 
 int main(int argc, char *argv[]) {
   // Semilla para la generacion de numeros aleatorios
   srand(time(0));
-
-  struct timeval inicio, final;
-
   int filas, columnas, idx_proceso, cant_procesos;
+  int i, j, sum = 0;
 
   // Obtencion de los parametros que se pasan en el llamado al ejecutable
-  filas = columnas =
-      atoi(argv[1]); // Debido a que se está trabajando con matrices cuadradas
+  filas = atoi(argv[1]);
+  columnas = filas;
 
-  MATRIZ_A = malloc_arreglo_2d_enteros(filas, columnas);
-  MATRIZ_B = malloc_arreglo_2d_enteros(filas, columnas);
-  RESULTADO = malloc_arreglo_2d_enteros(filas, columnas);
-
-  asignar_valores_aleatorios_matriz(filas, columnas, MATRIZ_A);
-  asignar_valores_aleatorios_matriz(filas, columnas, MATRIZ_B);
-
-  // NOTE: Solo medir el tiempo en la región de procesos
   MPI_Init(&argc, &argv);
 
   MPI_Comm_size(MPI_COMM_WORLD, &cant_procesos);
   MPI_Comm_rank(MPI_COMM_WORLD, &idx_proceso);
-  multiplicar_matrices(filas, columnas, cant_procesos, idx_proceso);
-  MPI_Barrier(MPI_COMM_WORLD);
+
+  int *matriz_a;
+
+  if (idx_proceso == 0) {
+    matriz_a = malloc_arreglo_1d_enteros(filas, columnas);
+    asignar_valores_aleatorios_arreglo(filas, columnas, matriz_a);
+    printf("Matriz inicial:\n");
+    mostrar_arreglo_como_matriz(filas, columnas, matriz_a);
+  }
+
+  int *matriz_a_temp = malloc((filas * columnas / cant_procesos) * sizeof(int));
+
+  MPI_Scatter(matriz_a, (filas * columnas / cant_procesos), MPI_INT,
+              matriz_a_temp, (filas * columnas / cant_procesos), MPI_INT, 0,
+              MPI_COMM_WORLD);
+
+  printf("El procesos %d recibió los siguientes elementos: ", idx_proceso);
+  mostrar_arreglo((filas * columnas / cant_procesos), matriz_a_temp);
+
   MPI_Finalize();
 
-  mostrar_matriz(filas, columnas, MATRIZ_A);
-  mostrar_matriz(filas, columnas, MATRIZ_B);
-  mostrar_matriz(filas, columnas, RESULTADO);
-
-  // Tomar el tiempo de ejecución
-  gettimeofday(&final, NULL);
-  double tiempo_ejecucion;
-  tiempo_ejecucion =
-      (final.tv_sec - inicio.tv_sec) + 1e-6 * (final.tv_usec - inicio.tv_usec);
-
-  printf("Multiplicando matrices de %i x %i\n", filas, columnas);
-  printf("Tiempo de ejecución: %f\n", tiempo_ejecucion);
-  printf("\n");
   return 0;
 }
